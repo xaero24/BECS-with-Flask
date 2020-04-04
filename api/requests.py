f7da0be5b2985718dbbe2208f5585027f5361050
Mike A <michaelafu@gmail.com>
2020-04-04 17:27:21 +0300

    Removed user validation, added some python

--- api/requests.py
+++ api/requests.py
@@ -1,45 +1,17 @@
 from flask import Flask, request, render_template
-
+import data.blood as bl
+from csv import reader, writer
 
 becs = Flask(__name__)
-
-@becs.route('/api/signup', methods=['POST'])
-def signup():
-    pass
-
-@becs.route('/api/login', methods=['POST'])
-def login():
-    pass
-
-@becs.route('/api/logout', methods=['POST'])
-def logout():
-    pass
-
-@becs.route('/api/checkSession/<uname>', methods=['POST'])
-def checkSession(uname):
-    pass
 
 @becs.route('/api/add_portion', methods=['POST'])
 def addPortion():
     data = request.get_json()
 
-@becs.route('/api/add_portions', methods=['POST'])
-def addPortions():
-    data = request.get_json()
-
-@becs.route('/api/get_portion', methods=['GET'])
+@becs.route('/api/get_portion', methods=['POST'])
 def getPortion():
     data = request.get_json()
 
-@becs.route('/api/get_portions', methods=['GET'])
+@becs.route('/api/get_portions', methods=['POST'])
 def getPortions():
     data = request.get_json()
-
-@becs.route('/<user>/<page>.html', methods=['GET'])
-def getUserPage(user, page):
-    # TODO: Add user check in session
-    return render_template(f'front/html/{page}.html', message=f'{user}')
-
-@becs.route('/<page>.html', methods=['GET'])
-def getPublicPage(page):
-    pass
--- api/templates/front/js/actions.js
+++ api/templates/front/js/actions.js
@@ -1,4 +1,35 @@
-function square(num) {
-    var sq = num * num;
-    document.getElementById("sq").innerHTML = sq;
+function sendBlood(bloodpack) {
+    var req = new XMLHttpRequest();
+    var url = "http://127.0.0.1:5000/api/add_portion";
+    req.open("POST", url);
+    req.send(bloodpack);
+    if (req.responseText == "True") {
+        alert("Submitted successfully.");
+    } else {
+        alert("An error occured: " + req.responseText);
+    }
+}
+
+function getBlood(bloodpack) {
+    var req = new XMLHttpRequest();
+    var url = "http://127.0.0.1:5000/api/get_portion";
+    req.open("POST", url);
+    req.send(bloodpack);
+    if (req.responseText == "True") {
+        alert("Pulled successfully.");
+    } else {
+        alert("An error occured: " + req.responseText);
+    }
+}
+
+function mciReport(bloodpacks) {
+    var req = new XMLHttpRequest();
+    var url = "http://127.0.0.1:5000/api/get_portions";
+    req.open("POST", url);
+    req.send(bloodpacks);
+    if (req.responseText == "True") {
+        alert("Pulled successfully.");
+    } else {
+        alert("An error occured: " + req.responseText);
+    }
 }
